269765d24d7d64569aba576183c81c5106fce8ad
thihaswe-work <thihaswe.work@gmail.com>
2024-12-13 09:49:18 +0630

    passionGeek: about us (responsive ui)

--- src/components/aboutUsPage/iconSection.tsx
+++ src/components/aboutUsPage/iconSection.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const IconSection = () => {
   const icons = [1, 2, 3, 4, 5];
   return (
-    <div className="w-full flex justify-center flex-wrap">
+    <div className="w-full flex justify-center flex-wrap my-[44px] md:my-[64px]">
       {icons.map((icon) => {
         return (
           <div className="text-center text-gray-50 font-semibold tracking-wide">
