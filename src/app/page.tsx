02c470fde01966f54367eccb3fb2cc3ee9e52320
Macmini <macmini@Macminis-Mac-mini.local>
2025-02-24 13:21:10 +0630

    home finshed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,24 +36,25 @@ export default function Home() {
         </div>
       </div>
       <HomeSwiper />
-      <div className="flex flex-col gap-10 border-b border-thirdBgColor pb-20">
+      <div className="flex flex-col gap-10  pb-20 ">
         <CustomMarquee />
         <CustomMarquee direction={true} />
         <CustomMarquee />
       </div>
-
-      <div className="container mx-auto flex justify-between">
-        <div className="max-w-[545px] flex flex-col justify-center">
-          <h1 className="font-bold text-[40px] ">
-            Get a booking schedule with us.
-          </h1>
-          <Button className="py-5 px-10 rounded-md bg-textColor w-fit h-fit flex gap-3">
-            <Image src={"/book.png"} alt="" width={10} height={10} />
-            <span> Book Now</span>
-          </Button>
-        </div>
-        <div className="relative w-[50%] max-w-[618px] h-[412px] ">
-          <Image src={"/three.svg"} alt="" fill />
+      <div className="border-b border-t border-thirdBgColor py-6">
+        <div className="container mx-auto flex justify-between ">
+          <div className="max-w-[545px] flex flex-col justify-center">
+            <h1 className="font-bold text-[40px] ">
+              Get a booking schedule with us.
+            </h1>
+            <Button className="py-5 px-10 rounded-md bg-textColor w-fit h-fit flex gap-3">
+              <Image src={"/book.png"} alt="" width={10} height={10} />
+              <span> Book Now</span>
+            </Button>
+          </div>
+          <div className="relative w-[50%] max-w-[618px] h-[412px] ">
+            <Image src={"/three.svg"} alt="" fill />
+          </div>
         </div>
       </div>
     </div>
